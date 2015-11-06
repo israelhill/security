141885db1cf860ff84d3c36523ab311ca7192277
Israel Hill <idh@case.edu>
2015-11-06 12:19:12 -0500

    printing to output file with correct format is working

--- src/LogReader.java
+++ src/LogReader.java
@@ -1,21 +1,16 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.IntSummaryStatistics;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LogReader {
     private int securityLimit;
-    private Pattern pattern;
-    private Matcher matcher;
     private int blanks;
     private HashMap<User, Integer> map = new HashMap<>();
     private String log;
-    private boolean reachedFailedLogins;
     private boolean reachedIllegalUsers;
     private static final LogReader logReader = new LogReader();
+    private ArrayList<String> blackList = new ArrayList<>();
 
     private static final String HOST_NAME_PATTERN = "\\(([a-z0-9]+(-[a-z0-9]+)*\\.)+[a-z]{2,}\\)\\:";
     private static final String IP_PATTERN = "(?:[0-9]{1,3}\\.){3}[0-9]{1,3}";
@@ -27,6 +22,7 @@ public class LogReader {
         this.securityLimit = 3;
     }
 
+    // get instance of class
     public LogReader getInstance() {
         return logReader;
     }
@@ -40,27 +36,24 @@ public class LogReader {
             logReader.readFile();
         }
         catch (IOException e) {
-            e.printStackTrace();
+            throw new InvalidFileException(e);
         }
+        logReader.writeBlackListToFile();
+    }
+
+    private static void setupStandardOutput() {
+        PrintStream output = null;
+        try {
+            output = new PrintStream(new FileOutputStream("black_list.txt"));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Input file not Found. Please place file in correct directory.");
+        }
+        System.setOut(output);
     }
 
     private void setPrams() {
         reachedIllegalUsers = false;
-        reachedFailedLogins = false;
-    }
-
-    private void readFile() throws IOException {
-        readSecurityLog();
-
-        System.out.println("\n" + "MAP:");
-        for(Map.Entry<User, Integer> entry : map.entrySet()) {
-            if(entry.getKey().hasHostName()) {
-                System.out.println(entry.getKey().getHostName() + ": " + entry.getValue());
-            }
-            else {
-                System.out.println(entry.getKey().getIp() + ": " + entry.getValue());
-            }
-        }
     }
 
     private void getLogAsString() throws IOException {
@@ -75,40 +68,71 @@ public class LogReader {
         log = builder.toString();
     }
 
-    private static void setupStandardOutput() {
-        PrintStream output = null;
-        try {
-            output = new PrintStream(new FileOutputStream("black_list.txt"));
-        }
-        catch (FileNotFoundException e) {
-           System.out.println("File not Found. Please place file in correct directory.");
-        }
-        System.setOut(output);
-    }
-
-    private void checkOrder(String line) {
-        if(line.equals("Failed logins from:")) {
-            reachedFailedLogins = true;
-            if(reachedIllegalUsers) {
-                //TODO throw an exception
-                System.out.println("Illegal users before failed logins!");
+    private void createBlackList() {
+        for(Map.Entry<User, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > securityLimit) {
+                if(entry.getKey().hasHostName()) {
+                    blackList.add(entry.getKey().getHostName());
+                }
+                else {
+                    blackList.add(entry.getKey().getIp());
+                }
             }
         }
-        else if(line.equals("Illegal users from:")) {
-            reachedIllegalUsers = true;
+
+        for(String s : blackList) {
+            System.out.println("Address: " + s);
+        }
+        System.out.println("\n");
+    }
+
+    private void writeBlackListToFile() {
+        createBlackList();
+        String host;
+        final String FIVE_BLANKS = "     ";
+        StringBuilder builder = new StringBuilder();
+        final int MAX_LINE_LENGTH = 79;
+
+        for(int i = 0; i < blackList.size(); i++) {
+            host = blackList.get(i) + ",";
+            if((builder.length() + host.length()) < MAX_LINE_LENGTH) {
+                builder.append(host);
+            }
+            else {
+                builder.append("\\");
+                System.out.println(FIVE_BLANKS + builder.toString());
+                builder = new StringBuilder();
+                builder.append(host);
+            }
+        }
+
+        if(builder.length() > 0) {
+            // One host left in builder. Remove the comma and print it without a back slash
+            String lastLine = builder.toString().substring(0, builder.length() - 1);
+            System.out.println(FIVE_BLANKS + lastLine);
         }
     }
 
-    private void checkFormat() {
-//        System.out.println("Condition: " + String.valueOf(!reachedIllegalUsers && blanks > 1) + "  Illegal" +
-//                " users: " + String.valueOf(reachedIllegalUsers) + " Blanks: " + blanks);
-        if(!reachedIllegalUsers && blanks > 1) {
-            //TODO throw an exception
-            throw new RuntimeException();
+    private void readFile() throws IOException {
+        try {
+            readSecurityLog();
+        } catch (InvalidSyntaxException e) {
+            throw new InvalidFileException(e);
         }
+
+        System.out.println("\n" + "MAP:");
+        for(Map.Entry<User, Integer> entry : map.entrySet()) {
+            if(entry.getKey().hasHostName()) {
+                System.out.println(entry.getKey().getHostName() + ": " + entry.getValue());
+            }
+            else {
+                System.out.println(entry.getKey().getIp() + ": " + entry.getValue());
+            }
+        }
+        System.out.println("\n\n");
     }
 
-    private void readSecurityLog() throws IOException {
+    private void readSecurityLog() throws IOException, InvalidSyntaxException {
         InputStream is = new ByteArrayInputStream(log.getBytes());
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
@@ -130,13 +154,30 @@ public class LogReader {
         br.close();
     }
 
-    private boolean readNextLine(String line) throws IOException {
+    private boolean readNextLine(String line) throws IOException, InvalidSyntaxException {
         if(line.isEmpty()) {
             blanks++;
             checkFormat();
         }
 
         return (blanks < 2) && !line.isEmpty() && !line.equals("Illegal users from:");
+    }
+
+    private void checkOrder(String line) throws InvalidSyntaxException {
+        if(line.equals("Failed logins from:")) {
+            if(reachedIllegalUsers) {
+                throw new InvalidSyntaxException("Illegal user section is before Failed logins");
+            }
+        }
+        else if(line.equals("Illegal users from:")) {
+            reachedIllegalUsers = true;
+        }
+    }
+
+    private void checkFormat() throws InvalidSyntaxException {
+        if(!reachedIllegalUsers && blanks > 1) {
+            throw new InvalidSyntaxException("Too many blank lines in file.");
+        }
     }
 
     private void isValidLine(String[] portions, int size) {
@@ -203,7 +244,7 @@ public class LogReader {
 
     //BARRICADE METHODS
 
-    private void validateThreeArgLine(String ip, String host, String count) throws InvalidSyntaxException {
+    public void validateThreeArgLine(String ip, String host, String count) throws InvalidSyntaxException {
         boolean validHost = isValidPattern(HOST_NAME_PATTERN, host);
         boolean validIp = isValidPattern(IP_PATTERN, ip);
         boolean validCount = isValidPattern(FAIL_COUNT_PATTERN, count);
@@ -219,7 +260,7 @@ public class LogReader {
         }
     }
 
-    private void validateTwoArgLine(String ip, String count) throws InvalidSyntaxException {
+    public void validateTwoArgLine(String ip, String count) throws InvalidSyntaxException {
         boolean validIp = isValidPattern(IP_PATTERN, ip);
         boolean validCount = isValidPattern(FAIL_COUNT_PATTERN, count);
 
@@ -234,9 +275,9 @@ public class LogReader {
         }
     }
 
-    private boolean isValidPattern(String regexPattern, String s) {
-        pattern = Pattern.compile(regexPattern);
-        matcher = pattern.matcher(s);
+    public boolean isValidPattern(String regexPattern, String s) {
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(s);
         return matcher.matches();
     }
 }
